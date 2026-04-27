f94caf65522c4ea307fafa90a2eab3ce7efedb6d
LAUGH <themlaka21@gmail.com>
2026-04-27 09:00:38 +0200

    l

--- app.py
+++ app.py
@@ -129,6 +129,14 @@ Rules:
     except Exception as e:
         return jsonify({"error": str(e)})
 
-
+def main():
+    conn = psycopg2.connect(database="mariadb_yri6",
+                            user="mariadb_yri6_user",
+                            password="J8sL1alyOxScUoV4rUOpYChvkwG2ZDSt",
+                            host="dpg-d7ng3t6gvqtc73ar4g00-a.frankfurt-postgres.render.com",
+                            port=5432)
+    
+    print("DATABASE CONNECTED!)
+                            
 if __name__ == "__main__":
     app.run()
