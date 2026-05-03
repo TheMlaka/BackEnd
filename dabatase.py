80e36fa7e5a2ca2a553c37d1472608b029f64e22
LAUGH <themlaka21@gmail.com>
2026-05-03 22:16:27 +0200

    d

--- dabatase.py
+++ dabatase.py
@@ -1,5 +1,12 @@
+import os
+import psycopg2
+
+def get_db():
+    return psycopg2.connect(os.environ.get("DATABASE_URL"))
+
+
 def create_table():
-    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
+    conn = get_db()
     cur = conn.cursor()
 
     cur.execute("""
@@ -17,4 +24,27 @@ def create_table():
     cur.close()
     conn.close()
 
-    print("TABLE READY")
+    print("✅ TABLE READY")
+
+
+def seed_data():
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM students;")
+    count = cur.fetchone()[0]
+
+    if count == 0:
+        cur.execute("""
+        INSERT INTO students (name, surname, nickname, personality, image)
+        VALUES
+        ('Rastislav','Paták','Kašlík','you are very shy','https://pixnio.com/free-images/2025/12/14/2025-12-14-14-41-59-576x576.jpg'),
+        ('Daniel','Barta','Bart','you love pcs and drones','https://d50-a.sdn.cz/d_50/c_img_F_C/3dGKgz.jpeg'),
+        ('Samuel','Martiš','Žukva','you love cats and music','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8bG2cU0TXNW')
+        ;
+        """)
+        conn.commit()
+        print("🔥 DATA SEEDED")
+
+    cur.close()
+    conn.close()
